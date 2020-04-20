1834ea07e3c25483bd9461b27c0c6d50a0374622
Adam Tuechler <adamtuechler@gmail.com>
2020-04-20 10:38:29 +1000

    Added interfaces for authentication

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -4,6 +4,9 @@ import {Controller} from './_controller';
 import {hashPassword} from '../lib/crypto';
 import {jwtConfig} from '../config';
 
+import {Player} from '../../models/player';
+import {LoginForm} from '../../models/auth';
+
 export class AuthController extends Controller {
 	applyRoutes(router: Router): void {
 		router.post('/login', this.playerPostLogin);
@@ -13,24 +16,27 @@ export class AuthController extends Controller {
 	/* Player attempts to login */
 	playerPostLogin = async (request: Request, response: Response): Promise<void> => {
 		console.log('POST /login');
+		const loginInfo: LoginForm = request.body;
 
 		// Hash the user password
-		const hashpwd = await hashPassword(request.body.password);
+		const hashpwd = await hashPassword(loginInfo.password);
 
 		// Retrieve player listing
-		const players = await this.api.postgresClient.query(
+		const playerQuery = await this.api.postgresClient.query(
 			'SELECT "id" FROM "player" WHERE "username"=$1 and "password"=$2;',
-			[request.body.username, hashpwd]
+			[loginInfo.username, hashpwd]
 		);
+
+		const players: Player[] = playerQuery.rows;
 
 		const authInfo = {
 			success: false,
 			token: ''
 		};
 
-		if (players.rows.length === 1) {
+		if (players.length === 1) {
 			const payload = {
-				id: players.rows[0].id
+				id: players[0].id
 			};
 
 			authInfo.token = jwt.sign(
